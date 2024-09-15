707d95668882f68e6e39167e55f2b48727b1c8f4
Harris Ransom <hransom528@gmail.com>
2024-09-15 14:14:06 -0400

    Added counter and shift register SystemVerilog examples

---
+++ counter.sv
@@ -0,0 +1,14 @@
+// Binary Counter
+// From Digital Design and Computer Architecture 2ed
+module counter #(parameter N=8)
+				(input	logic clk,
+				 input	logic reset,
+				 output logic [N-1:0] q);
+
+	always_ff @(posedge clk, posedge reset)
+		if (reset)
+			q <= 0
+		else
+			q <= q+1
+
+endmodule
